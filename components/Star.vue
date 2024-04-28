8628e608f3f344237f88d4a05803ef3bdb78a122
wannabeSE <samirk724@gmail.com>
2024-04-29 02:10:59 +0600

    add/ starry background animation

---
+++ components/Star.vue
@@ -0,0 +1,12 @@
+<template>
+  <TresMesh :position="[x, y, z]">
+    <TresSphereGeometry :args="[0.008, 22, 22]" />
+    <TresMeshBasicMaterial :color="'#FFFFFF'" />
+  </TresMesh>
+</template>
+<script setup>
+import { MathUtils } from "three";
+const [x, y, z] = Array(3)
+  .fill()
+  .map(() => MathUtils.randFloatSpread(10));
+</script>
--- pages/test/index.vue
+++ pages/test/index.vue
@@ -1,22 +1,24 @@
 <template>
+  <div class="w-full h-auto fixed inset-0 z-[20]">
     <TresCanvas window-size clear-color="#030014">
-        <TresPerspectiveCamera visible />
-        <TresMesh ref="starRef">
-            <TresSphereGeometry />
-            <TresMeshBasicMaterial :color="'#A39393'" />
-        </TresMesh>
+      <TresPerspectiveCamera :args="cameraArgs" :position="[0, 0, 5]" />
+      <TresGroup ref="starRef" :rotation-z="Math.PI / 4">
+        <Star v-for="i in stars" :key="i" />
+      </TresGroup>
     </TresCanvas>
+  </div>
 </template>
 <script setup>
-import { Scene, SphereGeometry, MeshBasicMaterial, Mesh, MathUtils } from 'three';
-import * as random from 'maath/random/dist/maath-random.esm';
-const starRef = ref()
+const starRef = ref();
+const cameraArgs = [75, window.innerHeight / window.innerHeight, 0.1, 1000];
 const { onLoop } = useRenderLoop();
+//Generating stars  
+const stars = Array.from({ length: 1500 }, (_, index) => index + 1);
+//animation
 onLoop(({ delta }) => {
-    if (starRef.value) {
-        starRef.value.rotation.x -= delta / 10;
-        starRef.value.rotation.y -= delta / 15
-    }
-})
-
+  if (starRef.value) {
+    starRef.value.rotation.x -= delta / 10;
+    starRef.value.rotation.y -= delta / 15;
+  }
+});
 </script>

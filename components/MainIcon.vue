fdc89f3aa9292e349134f0ba4a61f28227b964e8
wannabeSE <samirk724@gmail.com>
2024-05-03 03:16:34 +0600

    update/ mainIconsDark uploaded to cloudinary

--- components/MainIcon.vue
+++ components/MainIcon.vue
@@ -5,7 +5,11 @@
     :initial="slideFromRight.initial"
     :enter="slideFromRight.enter"
   >
-    <NuxtImg src="/mainiconsdark.svg" height="650" width="650" alt="logo" />
+    <NuxtImg
+    src="https://res.cloudinary.com/dcmgtfqoi/image/upload/v1714684368/mainIconsdark_ynspdo.svg" 
+    height="650" 
+    width="650" 
+    alt="logo" />
   </div>
 </template>
 

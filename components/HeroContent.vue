05b090e08d170339204f4d101229adc57546f268
wannabeSE <samirk724@gmail.com>
2024-05-02 21:35:09 +0600

    fix/ build issue

--- components/HeroContent.vue
+++ components/HeroContent.vue
@@ -41,7 +41,7 @@
         </div>
         <div class="w-full h-full flex justify-center items-center" v-motion :initial="slideFromRight.initial"
             :enter="slideFromRight.enter">
-            <NuxtImg src="/mainiconsdark.svg" sizes="100vw sm:250px md:400px" alt="logo"/>
+            <NuxtImg src="/mainiconsdark.svg" height="650" width="650" alt="logo"/>
         </div>
     </div>
 </template>
